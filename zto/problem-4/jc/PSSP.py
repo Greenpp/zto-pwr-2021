9ccbb76d7692c561c0664566c9faecaa49f30f3e
Jakub Ciszek <jakubciszek64@gmail.com>
2021-04-15 12:24:44 +0200

    Problem initialization

--- zto/problem-4/jc/PSSP.py
+++ zto/problem-4/jc/PSSP.py
@@ -1,8 +1,22 @@
+from ...RNG import RandomNumberGenerator
 from .bandb import Problem, Solution
 
 
 class PSSPProblem(Problem):
-    pass
+    def __init__(self, tasks: int, machines: int, rng_seed: int = 42) -> None:
+        self.tasks = tasks
+        self.machines = machines
+
+        rng = RandomNumberGenerator(rng_seed)
+        self.cost = [
+            [rng.nextInt(1, 99) for _ in range(machines)] for _ in range(tasks)
+        ]
+
+    def get_level_max(self, level: int):
+        raise Exception('It is a minimization problem')
+
+    def get_level_min(self, level: int):
+        pass
 
 
 class PSSPSolution(Solution):
