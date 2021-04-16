a471ce57d4f484931b44f11f4b2b596317bc33fd
Jakub Ciszek <jakubciszek64@gmail.com>
2021-04-16 13:16:10 +0200

    Task expansion

--- zto/problem-4/jc/PSSP.py
+++ zto/problem-4/jc/PSSP.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from ...RNG import RandomNumberGenerator
 from .bandb import Problem, Solution
 
@@ -12,12 +14,54 @@ class PSSPProblem(Problem):
             [rng.nextInt(1, 99) for _ in range(machines)] for _ in range(tasks)
         ]
 
-    def get_level_max(self, level: int):
-        raise Exception('It is a minimization problem')
+    def expand(self, solution: Optional['PSSPSolution']) -> list['PSSPSolution']:
+        if solution is None:
+            tasks_left = [i for i in range(self.tasks)]
+            sequence = []
+            time = [0 for _ in range(self.machines)]
+        else:
+            tasks_left = solution.tasks_left
+            sequence = solution.tasks_sequence
+            time = solution.work_time
 
-    def get_level_min(self, level: int):
+        new_solutions = []
+        for task in tasks_left:
+            new_machines_left = tasks_left.copy()
+            new_machines_left.remove(task)
+
+            new_sequence = sequence.copy()
+            new_sequence.append(task)
+
+            new_time = time.copy()
+            prev_machine_time = 0
+            for machine, t in enumerate(new_time):
+                new_time[machine] += (
+                    max(prev_machine_time, t) + self.cost[task][machine]
+                )
+                prev_machine_time = new_time[machine]
+
+            new_solution = PSSPSolution(new_sequence, new_machines_left, new_time)
+            new_solutions.append(new_solution)
+
+    def get_rand_solution(self) -> Solution:
+        pass
+
+    def get_greedy_solution(self) -> Solution:
+        pass
+
+    def get_dfs_solution(self) -> Solution:
         pass
 
 
 class PSSPSolution(Solution):
-    pass
+    def __init__(
+        self,
+        task_sequence: list[int],
+        tasks_left: list[int],
+        work_time: list[int],
+    ) -> None:
+        super().__init__(work_time[-1], is_final=not tasks_left)
+
+        self.tasks_sequence = task_sequence
+        self.tasks_left = tasks_left
+        self.work_time = work_time
