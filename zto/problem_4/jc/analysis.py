6391185a9620799de36f6e939329ac5c1dd90591
Jakub Ciszek <jakubciszek64@gmail.com>
2021-04-19 13:35:35 +0200

    Experiments setup

---
+++ zto/problem_4/jc/analysis.py
@@ -0,0 +1,37 @@
+from .environment import P4Environment
+from .experiment import Experiment, Lab
+
+
+def run_lab():
+    lab = Lab(
+        name='p4',
+        env_cls=P4Environment,
+        method='bb',
+        init_typ='none',
+        enqueue_limit=-1,
+        tasks=10,
+        machines=10,
+    )
+
+    e1 = Experiment(
+        name='bb_init_type',
+        var_name='init_type',
+        var_values=['none', 'rand', 'greedy', 'dfs'],
+    )
+    lab.add_experiment(e1)
+
+    e2 = Experiment(
+        name='bb_enq_lim',
+        var_name='enqueue_limit',
+        var_values=list(range(1, 11)),
+    )
+    lab.add_experiment(e2)
+
+    e3 = Experiment(
+        name='bf',
+        var_name='method',
+        var_values=['bf'],
+    )
+    lab.add_experiment(e3)
+
+    lab.run()
