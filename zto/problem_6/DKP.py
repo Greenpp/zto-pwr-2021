8e464c2bd26239e66d14882e9aba1233b2325d68
Jakub Ciszek <jakubciszek64@gmail.com>
2021-05-15 18:20:16 +0200

    Reformatted, solution comparison

--- zto/problem_6/DKP.py
+++ zto/problem_6/DKP.py
@@ -1,4 +1,5 @@
 import random
+from functools import total_ordering
 
 from ..RNG import RandomNumberGenerator as RNG
 
@@ -15,68 +16,100 @@ class DKProblem:
         # Pojemność
         self.b = self.rgen.nextInt(5 * items, 10 * items)
 
-    def evaluate_selection_value(self, selected: list[int]) -> int:
+    def evaluate_selection_value(self, selected: set[int]) -> int:
         value = sum([self.c[idx] for idx in selected])
 
         return value
 
-    def evaluate_selection_boundary(self, selected: list[int]) -> bool:
+    def evaluate_selection_weight(self, selected: set[int]) -> int:
         weight = sum([self.w[idx] for idx in selected])
 
+        return weight
+
+    def is_in_bounds(self, weight: int) -> bool:
         return weight <= self.b
 
+    def selected_to_solution(self, selected: set[int]) -> 'DKSolution':
+        value = self.evaluate_selection_value(selected)
+        weight = self.evaluate_selection_weight(selected)
+        in_bounds = self.is_in_bounds(weight)
+
+        new_solution = DKSolution(selected, value, weight, in_bounds)
+
+        return new_solution
+
+    def get_not_selected(self, selected: set[int]) -> list[int]:
+        not_selected = list(filter(lambda it: it not in selected, range(self.items)))
+
+        return not_selected
+
     def get_full_neighborhood(self, solution: 'DKSolution') -> list['DKSolution']:
-        not_selected = list(
-            filter(lambda it: it not in solution.selected, range(self.items))
-        )
+        not_selected = self.get_not_selected(solution.selected)
 
         nb_solutions = []
-        for i in range(len(solution.selected)):
+        for it in solution.selected:
             for n_it in not_selected:
                 new_selected = solution.selected.copy()
-                new_selected[i] = n_it
+                new_selected.remove(it)
+                new_selected.add(n_it)
 
-                new_value = self.evaluate_selection_value(new_selected)
-                new_in_bounds = self.evaluate_selection_boundary(new_selected)
-
-                new_solution = DKSolution(new_selected, new_value, new_in_bounds)
+                new_solution = self.selected_to_solution(new_selected)
                 nb_solutions.append(new_solution)
         for n_it in not_selected:
             new_selected = solution.selected.copy()
-            new_selected.append(n_it)
+            new_selected.add(n_it)
 
-            new_value = self.evaluate_selection_value(new_selected)
-            new_in_bounds = self.evaluate_selection_boundary(new_selected)
-
-            new_solution = DKSolution(new_selected, new_value, new_in_bounds)
+            new_solution = self.selected_to_solution(new_selected)
             nb_solutions.append(new_solution)
 
         return nb_solutions
 
     def get_random_neighbor(self, solution: 'DKSolution') -> 'DKSolution':
-        not_selected = list(
-            filter(lambda it: it not in solution.selected, range(self.items))
-        )
+        not_selected = self.get_not_selected(solution.selected)
 
         new_item = random.choice(not_selected)
-        replace_idx = random.randint(0, len(solution.selected))
+        replace_it = random.choice(list(solution.selected | {None}))
 
         new_selected = solution.selected.copy()
-        if replace_idx == len(new_selected):
-            new_selected.append(new_item)
-        else:
-            new_selected[replace_idx] = new_item
+        if replace_it is not None:
+            new_selected.remove(replace_it)
 
-        new_value = self.evaluate_selection_value(new_selected)
-        new_in_bounds = self.evaluate_selection_boundary(new_selected)
+        new_selected.add(new_item)
 
-        new_solution = DKSolution(new_selected, new_value, new_in_bounds)
+        new_solution = self.selected_to_solution(new_selected)
 
         return new_solution
 
+    def get_random_solution(self, allow_out_of_bounds: bool = False) -> 'DKSolution':
+        s_len = random.randint(1, self.items)
 
+        selected = set(random.sample(range(self.items), s_len))
+        solution = self.selected_to_solution(selected)
+        if not allow_out_of_bounds:
+            while not solution.in_bounds:
+                s_len = random.randint(1, self.items)
+                selected = set(random.sample(range(self.items), s_len))
+                solution = self.selected_to_solution(selected)
+
+        return solution
+
+
+@total_ordering
 class DKSolution:
-    def __init__(self, selected: list[int], value: int, in_bounds: bool) -> None:
+    def __init__(
+        self, selected: set[int], value: int, weight: int, in_bounds: bool
+    ) -> None:
         self.selected = selected
         self.value = value
+        self.weight = weight
         self.in_bounds = in_bounds
+
+    def __gt__(self, other: 'DKSolution') -> bool:
+        if self.in_bounds and other.in_bounds:
+            return self.value > other.value
+        elif self.in_bounds and not other.in_bounds:
+            return True
+        elif not self.in_bounds and other.in_bounds:
+            return False
+        else:
+            return self.weight < other.weight
