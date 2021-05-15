8dbc9e944861fce50bd69d888e151beddebaf30e
Jakub Ciszek <jakubciszek64@gmail.com>
2021-05-15 18:26:39 +0200

    No progress limit and iteration limit

--- zto/problem_6/ABC.py
+++ zto/problem_6/ABC.py
@@ -4,16 +4,28 @@ from .DKP import DKProblem, DKSolution
 
 
 class ABCSolver:
-    def __init__(self, population: int, change_limit: int) -> None:
+    def __init__(
+        self,
+        population: int,
+        change_limit: int,
+        iteration_limit: int = 1000,
+        no_progress_limit: int = -1,
+    ) -> None:
         self.population = population
         self.change_limit = change_limit
+        self.iteration_limit = iteration_limit
+        self.no_progress_limit = no_progress_limit
 
     def update_best(self, solution: DKSolution) -> None:
         if solution > self.best_solution:
             self.best_solution = solution
 
     def should_stop(self) -> bool:
-        return self.iteration > 100
+        if self.iteration_limit <= self.iteration:
+            return True
+        if self.no_progress_limit > 0 and self.no_progress_limit <= self.no_progress:
+            return True
+        return False
 
     def try_solution_neighbor(
         self, problem: DKProblem, solution: 'ABCSolution'
@@ -21,6 +33,7 @@ class ABCSolver:
         nb_solution = problem.get_random_neighbor(solution.solution)
         if nb_solution > solution.solution:
             solution.replace_solution(nb_solution)
+            self.no_progress = 0
             self.update_best(nb_solution)
         else:
             solution.fail()
@@ -31,12 +44,14 @@ class ABCSolver:
         ]
 
         self.iteration = 0
+        self.no_progress = 0
 
         self.best_solution = solutions[0].solution
         for s in solutions:
             self.update_best(s.solution)
         while not self.should_stop():
             self.iteration += 1
+            self.no_progress = +1
             print(f'Iteration: {self.iteration}')
             # Zbieracze
             for s in solutions:
@@ -54,6 +69,7 @@ class ABCSolver:
                 if s.c > self.change_limit:
                     new_solution = problem.get_random_solution()
                     s.replace_solution(new_solution)
+                    self.no_progress = 0
                     self.update_best(new_solution)
 
         return self.best_solution
