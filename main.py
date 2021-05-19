94fa4cacb5b985c28c00f8333e9b09032a0e46ea
Jakub Ciszek <jakubciszek64@gmail.com>
2021-05-19 10:00:59 +0200

    Seeds added to showcase

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # %%
+from random import seed
+
 from zto.problem_6.ABC import ABCSolver
 from zto.problem_6.DKP import DKProblem
-from random import seed
 
 seed(42)
 
@@ -13,9 +14,14 @@ p.visualize()
 sol = s.solve(p)
 sol.visualize()
 
+
 # %%
-from zto.problem_6.sphere_problem import SphereProblem
+from random import seed
+
 from zto.problem_6.PSO import PSOSolver
+from zto.problem_6.sphere_problem import SphereProblem
+
+seed(42)
 
 s = PSOSolver(100, 0.1, 0.5, 0.5, 0.5, 1000, 100)
 p = SphereProblem(5)
